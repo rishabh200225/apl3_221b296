17faf3e0d5c9c601edb0310eef86fd84ad019da7
221B296 <221B296@juet.co.in>
2025-01-29 15:25:11 +0530

    simple

---
+++ lab02/prob1/Voice.java
@@ -0,0 +1,21 @@
+class Voice {
+ void prepareVoice() 
+    
+
+    {
+         Animal[] animals = new Animal[6];
+        animals[0] = new Cow();
+        animals[1] = new Dog();
+        animals[2] = new Pig();
+        animals[3] = new Goat();
+        animals[4] = new Lion();		
+	animals[5] = new Tiger();
+	
+	hear(animals);
+    }
+    void hear(Animal[] animal){
+ 	for(int i=0;i<6;i++){
+	animal[i].makeVoice();
+}
+}
+}
